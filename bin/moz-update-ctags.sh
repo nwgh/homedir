7349195d5d6833716cb258329b5c4b981e343f0c
Nick Hurley <hurley@mozilla.com>
2011-04-20 09:21:40 -0700

    [bin] Make etags go in regular ctags file

--- bin/moz-update-ctags.sh
+++ bin/moz-update-ctags.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-ctags -e -R --langmap=C++:+.idl --langmap=C++:+.ipdl \
+ctags -e -R -f tags --langmap=C++:+.idl --langmap=C++:+.ipdl \
     --languages=C,C++,HTML,JavaScript,Make,Python --extra=+q \
     --exclude=@${HOME}/Dropbox/moz/mc-ctags-exclude > /dev/null 2>&1
