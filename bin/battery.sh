f60a63675c93fdf2a114997e94b22aeedbf8b0b8
Nick Hurley <hurley@todesschaf.org>
2011-12-01 09:34:55 -0800

    Fancier battery stuff

--- bin/battery.sh
+++ bin/battery.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-/usr/sbin/ioreg -l | /usr/bin/grep -i capacity | /usr/bin/tr '\n' ' | ' | /usr/bin/awk '{printf("%.1f%%", $10/$5 * 100)}'
+IOR=$(/usr/sbin/ioreg -l)
+MAX=$(echo "$IOR" | /usr/bin/grep MaxCapacity | /usr/bin/cut -d= -f2 | /usr/bin/sed -e 's/ //g')
+CUR=$(echo "$IOR" | /usr/bin/grep CurrentCapacity | /usr/bin/cut -d= -f2 | /usr/bin/sed -e 's/ //g')
+EXT=$(echo "$IOR" | /usr/bin/grep ExternalConnected | /usr/bin/cut -d= -f2 | /usr/bin/sed -e 's/ //g')
+MIN=$(echo "$IOR" | /usr/bin/grep TimeRemaining | /usr/bin/cut -d= -f2 | /usr/bin/sed -e 's/ //g')
+echo $CUR $MAX $EXT $MIN
