843526d9a43b4b9dcfb2cc3ffb4d0bea6a42a718
Nick Hurley <hurley@todesschaf.org>
2013-02-20 17:03:18 -0800

    Add syntastic for vim (plus support)
    This lets me have better, more consistent syntax checking. Now if only
    I can get YouCompleteMe to work, I can kill clang_complete, and things
    will be totally froody in syntax checking land!

--- dotfiles/zsh.d/packages/000_homebrew.zsh
+++ dotfiles/zsh.d/packages/000_homebrew.zsh
@@ -1,6 +1,9 @@
 if [ -d /usr/local/share/python ] ; then
     export PATH=/usr/local/share/python:$PATH
 fi
+if [ -d /usr/local/share/npm/bin ] ; then
+    export PATH=/usr/local/share/npm/bin:$PATH
+fi
 if [ -d /usr/local/share/man ] ; then
     export MANPATH=/usr/local/share/man:$MANPATH
 fi
