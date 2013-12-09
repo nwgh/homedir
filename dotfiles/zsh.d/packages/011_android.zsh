fd188ede48d1995771caaff12a5ba5d951ad24c3
Nicholas Hurley <hurley@todesschaf.org>
2013-12-09 12:45:27 -0800

    Update android path, etc setup

--- dotfiles/zsh.d/packages/011_android.zsh
+++ dotfiles/zsh.d/packages/011_android.zsh
@@ -1,9 +1,13 @@
 if [ -d /opt/android ] ; then
     if [ -d /opt/android/tools ] ; then
-        export PATH=/opt/android/tools:$PATH
+        PATH=/opt/android/tools:$PATH
+    fi
+    if [ -d /opt/android/build-tools/18.1.1 ] ; then
+        PATH=/opt/android/build-tools/18.1.1:$PATH
     fi
     if [ -d /opt/android/platform-tools ] ; then
         PATH=/opt/android/platform-tools:$PATH
     fi
     export ANDROID_SDK=/opt/android
+    export PATH
 fi
