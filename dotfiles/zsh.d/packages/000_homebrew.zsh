5c0d407d3d672d1da21e079ede27f5ca5156e95a
Nicholas Hurley <hurley@todesschaf.org>
2013-12-24 11:08:06 -0800

    Remove /usr/local/share/python from path

--- dotfiles/zsh.d/packages/000_homebrew.zsh
+++ dotfiles/zsh.d/packages/000_homebrew.zsh
@@ -1,6 +1,3 @@
-if [ -d /usr/local/share/python ] ; then
-    export PATH=/usr/local/share/python:$PATH
-fi
 if [ -d /usr/local/share/npm/bin ] ; then
     export PATH=/usr/local/share/npm/bin:$PATH
 fi
