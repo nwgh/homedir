22ce83d3d04de8cd02d54aa78429396d3f541e6f
Nicholas Hurley <hurley@todesschaf.org>
2016-09-09 06:38:16 -0700

    Remove dynamic shell title with vcs info

--- dotfiles/zsh.d/prompt.zsh
+++ dotfiles/zsh.d/prompt.zsh
@@ -1,6 +1,4 @@
 source $HOME/.zsh.d/prompt_functions.zsh
 
-precmd_functions+="async_term_title"
-export precmd_functions
 export PROMPT="\$(make_pwdprompt)%(!.#.>) "
 export RPROMPT="\$(make_sshprompt)"
--- dotfiles/zsh.d/tmux-status.zsh
+++
@@ -1,7 +0,0 @@
-source $HOME/.zsh.d/prompt_functions.zsh
-
-cd "$(tmux display-message -p -F "#{pane_current_path}" -t0)"
-VIRTUAL_ENV="$(tmux display-message -p -F "#T" -t0)"
-export VIRTUAL_ENV
-statusline="$(TMUX="" make_topline)"
-echo "$statusline"
