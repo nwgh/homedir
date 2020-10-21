73ea57863143c0d74d401179f5c1e6b0aa8e441b
Nicholas Hurley <nwgh@nwgh.org>
2020-10-20 17:32:43 -0700

    Write manifest and try to remove outdated links on upgrade

--- bin/setup_homedir.py
+++ bin/setup_homedir.py
@@ -32,8 +32,23 @@ p = getattr(__builtins__, "print")
 e = functools.partial(p, file=sys.stderr)
 
 
-def __listdir(directory):
-    """Like os.listdir, but strips out any files beginning with .
+def __dir_sort(a, b):
+    """Comparator to sort a list of paths such that any file or subdirectory of
+    directory D comes before D in the sorted list.
+    """
+    if a.startswith(b):
+        return -1
+    elif b.startswith(a):
+        return 1
+    elif a < b:
+        return -1
+    elif b < a:
+        return 1
+    return 0
+
+
+def __listdir_nodot(directory):
+    """Like os.listdir, but omits any files beginning with .
     """
     files = os.listdir(directory)
     return [f for f in files if not f.startswith('.')]
@@ -42,20 +57,27 @@ def __listdir(directory):
 def __safelink(src, dst, act=True, verbose=False):
     """Like os.symlink, but does't behave badly if dst already exists.
     Also, ensures parent directory exists.
+
+    Returns a set of all the paths it created.
     """
+    created_paths = set()
     if not os.path.exists(dst):
         dirname = os.path.dirname(dst)
         if not os.path.exists(dirname):
             if verbose:
                 p('mkdir %s' % (dirname,))
             if act:
+                created_paths.add(dirname)
                 os.makedirs(dirname)
         if verbose:
             p('%s -> %s' % (dst, src))
         if act:
+            created_paths.add(dirname)
             os.symlink(src, dst)
     elif verbose:
         p('%s already exists, not creating link' % (dst,))
+
+    return created_paths
 
 
 def setup_homedir(destdir, srcdir, act=True, verbose=False):
@@ -74,18 +96,20 @@ def setup_homedir(destdir, srcdir, act=True, verbose=False):
     if not os.path.isdir(srcdir):
         raise Exception('srcdir must be a directory')
 
+    created_paths = set()
     dotfiledir = os.path.join(srcdir, 'dotfiles')
     if os.path.exists(dotfiledir) and os.path.isdir(dotfiledir):
-        dotfiles = __listdir(dotfiledir)
+        dotfiles = __listdir_nodot(dotfiledir)
         for f in dotfiles:
             dst = os.path.join(destdir, '.%s' % (f,))
             src = os.path.join(dotfiledir, f)
-            __safelink(src, dst, act=act, verbose=verbose)
+            created_paths.union(__safelink(src, dst, act=act, verbose=verbose))
     elif verbose:
         p('No dotfiledir found')
 
     source_stamp = os.path.join(destdir, '.homedir_setup_source')
     if act:
+        created_paths.add(source_stamp)
         with open(source_stamp, 'w') as f:
             f.write(srcdir)
         prefix = 'Wrote'
@@ -93,6 +117,32 @@ def setup_homedir(destdir, srcdir, act=True, verbose=False):
         prefix = 'Would write'
     if verbose:
         p('%s %s to %s' % (prefix, srcdir, source_stamp))
+
+    manifest = os.path.join(srcdir, '.manifest')
+    if act:
+        if os.path.exists(manifest):
+            with open(manifest) as f:
+                paths = f.read().strip().split('\n')
+            paths = set(paths)
+
+            # Get rid of links that are no longer part of the homedir. If it's
+            # a directory, remove it only if it's empty.
+            old_paths = list(paths.difference(created_paths))
+            old_paths.sort(cmp=__dir_sort)
+            for path in paths:
+                if os.path.islink(path) and not os.path.exists(path):
+                    os.unlink(path)
+                elif os.path.isdir(path):
+                    files = os.listdir(d)
+                    if not files:
+                        os.rmdir(d)
+
+        created_paths = list(created_paths)
+        created_paths.sort(cmp=__dir_sort)
+        with open(manifest, 'w') as f:
+            f.write('\n'.join(created_paths))
+    if verbose:
+        p('%s manifest to %s' % (prefix, manifest))
 
 
 if __name__ == '__main__':
