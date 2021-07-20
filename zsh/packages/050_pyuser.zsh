1798f9401333f827f891c51ed8c42fa024053706
Nicholas Hurley <nwgh@nwgh.org>
2021-07-20 15:03:03 -0700

    Handle py38 for user-level stuff

--- zsh/packages/050_pyuser.zsh
+++ zsh/packages/050_pyuser.zsh
@@ -1,4 +1,6 @@
-if [ -d "${HOME}/Library/Python/3.7/bin" ] ; then
+if [ -d "${HOME}/Library/Python/3.8/bin" ] ; then
+    export PATH="${PATH}:${HOME}/Library/Python/3.8/bin"
+elif [ -d "${HOME}/Library/Python/3.7/bin" ] ; then
     export PATH="${PATH}:${HOME}/Library/Python/3.7/bin"
 # TODO - handle linux local path
 fi
