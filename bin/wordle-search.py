607807c4871ba7c1c549137cf719588b8067c8a6
Nicholas Hurley <nwgh@nwgh.org>
2022-02-07 08:53:35 -0800

    Add script to help with wordle

---
+++ bin/wordle-search.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+import sys
+
+
+def die(msg):
+    print(msg)
+    sys.exit(1)
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--contains')
+parser.add_argument('--not-contains')
+parser.add_argument('pattern')
+args = parser.parse_args()
+
+if args.contains and not args.contains.isalpha():
+    die(f'Contains list must be letters a-z: {args.contains}')
+if args.not_contains and not args.not_contains.isalpha():
+    die(f'Does not contain list must be letters a-z: {args.not_contains}')
+
+if args.contains and args.not_contains:
+    overlap = set(args.contains).intersection(set(args.not_contains))
+    if overlap:
+        print('Contains and does not contain lists share one or more elements:')
+        die(''.join(c for c in overlap))
+
+pipeline = [f"grep -i '^{args.pattern}$' /usr/share/dict/words"]
+if args.contains:
+    for c in args.contains:
+        pipeline.append(f'grep {c}')
+if args.not_contains:
+    for c in args.not_contains:
+        pipeline.append(f'grep -v {c}')
+
+cmd = ' | '.join(pipeline)
+os.system(cmd)
