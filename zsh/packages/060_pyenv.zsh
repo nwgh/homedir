45b46dc8e09c827fff1920b56e6caae2022b2aa6
Nicholas Hurley <nicholas@forpeople.us>
2022-09-13 15:19:33 -0700

    Fix pyenv homebrew install

--- zsh/packages/060_pyenv.zsh
+++ zsh/packages/060_pyenv.zsh
@@ -2,6 +2,8 @@ if [[ -f "${HOME}/.pyenv/bin/pyenv" ]] ; then
     export PYENV_ROOT="${HOME}/.pyenv"
     export PATH="${PYENV_ROOT}/bin:${PATH}"
     eval "$(pyenv init --path)"
+elif [[ -f "/usr/local/bin/pyenv" ]] ; then
+    eval "$(pyenv init --path)"
 fi
 if which pyenv > /dev/null 2>&1 ; then
     eval "$(pyenv init -)"
