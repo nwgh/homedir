cc4ec2be11d1b63c5d6d6117c5d9f98a19be4956
Nicholas Hurley <nicholas@forpeople.us>
2025-04-16 13:18:58 -0700

    Better update for homebrew location

--- zsh/packages/000_homebrew.zsh
+++ zsh/packages/000_homebrew.zsh
@@ -4,8 +4,10 @@ fi
 if [ -d /usr/local/share/man ] ; then
     export MANPATH=/usr/local/share/man:$MANPATH
 fi
-if [ -d /home/linuxbrew/.linuxbrew/bin ] ; then
-    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-elif [ -d /opt/homebrew/bin ] ; then
+if [ -x /opt/homebrew/bin/brew ] ; then
     eval "$(/opt/homebrew/bin/brew shellenv)"
+elif [ -x /usr/local/bin/brew ] ; then
+    eval "$(/usr/local/bin/brew shellenv)"
+elif [ -x /home/linuxbrew/.linuxbrew/bin/brew ] ; then
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 fi
